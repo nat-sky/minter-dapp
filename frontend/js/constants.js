d27a8c9dd8d0c5a5e1d0fedac736443aca7af83c
nat-sky <nate.bragg420@gmail.com>
2022-04-24 13:52:09 +0100

    test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x5ec4eA284763fC05D7359CdeC0F53ffC68FDd24D"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
