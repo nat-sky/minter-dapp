275660397a9e8d85d4a5b22051a1fb4254c4a521
nat-sky <nate.bragg420@gmail.com>
2022-04-29 15:00:21 +0100

    derp

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the NiftyNoodles NFT Project!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'Nifty Noodles - the cutest creatures embedded in the Ethereum blockchain';
+const welcome_p = '';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
